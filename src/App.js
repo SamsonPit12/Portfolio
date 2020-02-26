77043927faf24a11782303a6da94aa284d2e5591
SamsonPit12 <edressler6@gmail.com>
2020-02-26 18:10:27 -0500

    added contact form and functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Header from './Components/Header/Header'
+// import Header from './Components/Header/Header'
 import "bootstrap/dist/css/bootstrap.min.css";
+import MyForm from './Components/Contactform/ContactForm';
 
 
 
@@ -10,9 +11,11 @@ function App() {
   return (
     <div className="App">
       
-    <Header />
-
+    {/* <Header /> */}
+    <MyForm />
+    
     </div>
+
   );
 }
 
