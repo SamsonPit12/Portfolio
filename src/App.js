53afe34f2ab847ea512658576fce1b5bd32515b6
SamsonPit12 <edressler6@gmail.com>
2020-02-26 13:23:58 -0500

    created navbar in Header folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
 
 function App() {
   return (
     <div className="App">
       
-    Hello
+    <Header />
 
     </div>
   );
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Nav, Navbar} from 'react-bootstrap';
+// import logo from './../../logo.svg';
+
+
+class Header extends React.Component {
+    render(){
+        return(
+            <Navbar bg="light" expand="lg" fixed='top'>
+                <Navbar.Brand href="#home">      
+                {/* <img
+                    src={logo}
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                    alt="logo"
+                />  */}
+                Eric</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/link">Link</Nav.Link>
+                        <Nav.Link href="/link">Link</Nav.Link>
+                        <Nav.Link href="/link">Link</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
+}
+
+export default Header;
