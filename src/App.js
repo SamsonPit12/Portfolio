29c6e1af9f870be139d3409fc28eef853436caed
SamsonPit12 <edressler6@gmail.com>
2020-03-26 17:07:18 -0400

    added landing and sidebar components to app.js

--- src/App.js
+++ src/App.js
@@ -3,26 +3,27 @@ import './Styles/App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from 'react-bootstrap';
 
-import NavbarHeader from './Components/Navbar/Navbar'
-import About from './Components/Body/About'
+import NavbarHeader from './Components/Navbar/Navbar';
+import Landing from './Components/Body/Landing';
+import About from './Components/Body/About';
 import ProjectCards from './Components/Body/ProjectCards';
 import ContactForm from './Components/Body/ContactForm';
-
+import SideContainer from './Components/Body/Sidebar';
 
 
 
 function App() {
   return (
-
-      <Container className="App">
-        <NavbarHeader /> 
+    <div className="App">
+      <SideContainer />
+      <Container style={{marginLeft: '35vh'}}>
+        <NavbarHeader />
+        <Landing />
         <About />
         <ProjectCards />
         <ContactForm />
-
       </Container>
-
-    
+    </div>
   );
 }
 
